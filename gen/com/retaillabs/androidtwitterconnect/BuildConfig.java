b071d30e5b84f713013f1ae1b79398e5fa04d437
Gajraj,Sachin <sachingajraj@gmail.com>
2014-10-31 02:37:33 +0530

    Android Twitter Connect basic app

---
+++ gen/com/retaillabs/androidtwitterconnect/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.retaillabs.androidtwitterconnect;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
